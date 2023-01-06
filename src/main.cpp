0db7798f592ffd082041d2018084b80af156d79c
Natesh Narain <nnaraindev@gmail.com>
2023-01-06 02:07:45 +0000

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -49,13 +49,6 @@ protected:
     // This function gets called every time a value is written to the local object
     // dictionary by an SDO or RPDO.
     void OnWrite(uint16_t idx, uint8_t subidx) noexcept override {
-        // if (idx == 0x4000 && subidx == 0) {
-        //     // Read the value just written to object 4000:00, probably by RPDO 1.
-        //     uint32_t val = (*this)[0x4000][0];
-        //     // Copy it to object 4001:00, so that it will be sent by the next TPDO.
-        //     (*this)[0x4001][0] = val;
-        // }
-        // lua.script("OnWrite()")
         lua["OnWrite"](idx, subidx);
     }
 
