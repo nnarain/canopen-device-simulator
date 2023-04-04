b4116c05d5e7c2f6dc22de5f4e32f73152f1ea0e
Natesh Narain <nnaraindev@gmail.com>
2023-04-04 00:03:20 -0400

    Update pdo mapping for example EDS

--- device/device.lua
+++ device/device.lua
@@ -11,6 +11,6 @@ end
 
 function OnWrite(index, subindex)
     value = GetU32(0x4000, 0x00)
-    print('Object ' .. string.format("%4x", index) .. ":" .. subindex .. " was updated to " .. GetU32(0x4000, 0x00))
+    print('Object ' .. string.format("%4x", index) .. "." .. subindex .. " was updated to " .. GetU32(0x4000, 0x00))
     SetU32(0x4001, 0x00, value + 1)
 end
